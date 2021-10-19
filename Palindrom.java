2a6f304e63b61cf8f3775272d41c66fd343fa2e0
przemyslaw-junior <78381232+przemyslaw-junior@users.noreply.github.com>
2021-10-19 20:49:08 +0200

    Add files via upload

---
+++ Palindrom.java
@@ -0,0 +1,19 @@
+package PrzemekT.ZadanieRekrutacyjne;
+
+public class Palindrom {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("kajak"));
+    }
+
+    public static boolean isPalindrom(String word){
+        int n = word.length();
+        for (int i=0; i<(n/2); i++){
+            if (word.charAt(i) != word.charAt(n-i-1)){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
